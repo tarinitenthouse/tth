87bb10bf69d9e8091bdf4757a7c76a6bdb2bbcb6
tarinitenthouse <tarinitenthouse.in@gmail.com>
2025-01-02 14:47:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 4000;
 
 // for handling cors
 const cors_options = {
-    origin: "https://tth.000.pe", // public URL
+    origin: "https://tth.my-style.in", // public URL
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     methods: "POST",
     credentials: true,
